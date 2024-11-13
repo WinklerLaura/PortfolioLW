aaf88229085454aa6867ee7938e5dc0d6b138beb
lwin <winkler@cogvis.ai>
2024-11-13 18:21:56 +0100

    adds Box with Card and Cardmedia to ProjectPage to display projects

--- src/components/pages/projectsPage/ProjectsPage.tsx
+++ src/components/pages/projectsPage/ProjectsPage.tsx
@@ -1,27 +1,79 @@
 import { PageLayout } from "../PageLayout";
+import { Box, Card, CardMedia } from "@mui/material";
 import headerProjects from "../../../assets/headerGraphics/headerProjects.svg";
-import styles from "../Pages.module.css";
-import { Box } from "@mui/material";
+import project1 from "../../../assets/projectImages/project1.svg";
 
 export const ProjectsPage = () => {
-    const boxStyle = {
-        width: "20vw",
-        height: "20vh",
-        bgcolor: "rgb(221, 213, 201, 0.04)",
-        padding: "20px",
+    const cardStyle = {
+        width: "630px",
+        height: "230px",
+        minHeight: "230px",
         margin: "2vh 2vw",
+        padding: "15px 15px",
         borderRadius: "25px",
         boxShadow: "2px 2px rgba(0, 0, 0, 0.1)",
+        bgcolor: "rgb(221, 213, 201, 0.04)",
+    };
+
+    const parentCardStyle = {
+        maxWidth: "40vw",
+        maxHeight: "60vh",
+        bgcolor: "rgb(221, 213, 201, 0.04)",
+        padding: "1.2rem",
+        borderRadius: "25px",
+        boxShadow: "2px 2px rgba(0, 0, 0, 0.1)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        overflowX: "auto",
+        "&::-webkit-scrollbar": {
+            width: "8px",
+            backgroundColor: "transparent",
+        },
+        "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(0, 0, 0, 0.2)",
+            borderRadius: "50px",
+        },
     };
 
     return (
         <div style={{ height: "100%" }}>
             <PageLayout image={headerProjects}>
-                <div className={styles.projectsContainer}>
-                    <Box sx={boxStyle}>Project1</Box>
-                    <Box sx={boxStyle}>Project2</Box>
-                    <Box sx={boxStyle}>Project3</Box>
-                </div>
+                <Box sx={parentCardStyle}>
+                    <Card sx={cardStyle}>
+                        <CardMedia
+                            sx={{
+                                height: 200,
+                                width: "100%",
+                                objectFit: "contain",
+                            }}
+                            image={project1}
+                            title="project1Image"
+                        />
+                    </Card>
+                    <Card sx={cardStyle}>
+                        <CardMedia
+                            sx={{
+                                height: 200,
+                                width: "100%",
+                                objectFit: "contain",
+                            }}
+                            image={project1}
+                            title="project1Image"
+                        />
+                    </Card>
+                    <Card sx={cardStyle}>
+                        <CardMedia
+                            sx={{
+                                height: 200,
+                                width: "100%",
+                                objectFit: "contain",
+                            }}
+                            image={project1}
+                            title="project1Image"
+                        />
+                    </Card>
+                </Box>
             </PageLayout>
         </div>
     );
