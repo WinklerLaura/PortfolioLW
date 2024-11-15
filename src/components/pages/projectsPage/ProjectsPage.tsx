dea4e17cdacf45ca55c49b9b3d6185cc4a55c9cc
lwin <winkler@cogvis.ai>
2024-11-15 21:48:58 +0100

    adds last styling touches to make project media cards repsonsive

--- src/components/pages/projectsPage/ProjectsPage.tsx
+++ src/components/pages/projectsPage/ProjectsPage.tsx
@@ -1,81 +1,41 @@
 import { PageLayout } from "../PageLayout";
-import { Box, Card, CardMedia } from "@mui/material";
+import { Box, CardContent, CardMedia } from "@mui/material";
 import headerProjects from "../../../assets/headerGraphics/headerProjects.svg";
 import project1 from "../../../assets/projectImages/project1.svg";
+import { cardMediaStyle, parentCardStyle } from "./cardStyles";
 
 export const ProjectsPage = () => {
-    const cardStyle2 = {
-        height: "30vh",
-        width: "28vw",
-        margin: "1vh 0.3vw",
-        borderRadius: "25px",
-        boxShadow: "2px 2px rgba(0, 0, 0, 0.1)",
-        bgcolor: "rgb(221, 213, 201, 0.04)",
-    };
-
-    const cardMediaStyle2 = {
-        height: "13vh",
-        width: "26vw",
-        margin: "2vh 1vw",
-        borderRadius: "25px",
-        objectFit: "contain",
-    };
-
-    const parentCardStyle = {
-        maxWidth: { xs: "100%", sm: "40vw" },
-        maxHeight: "60vh",
-        bgcolor: "rgb(221, 213, 201, 0.04)",
-        padding: "1.2rem",
-        borderRadius: "25px",
-        boxShadow: "2px 2px rgba(0, 0, 0, 0.1)",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        overflowX: "auto",
-        "&::-webkit-scrollbar": {
-            width: "8px",
-            backgroundColor: "transparent",
-        },
-        "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "rgba(0, 0, 0, 0.2)",
-            borderRadius: "50px",
-        },
-    };
+    const projectCards = [
+        { id: 1, image: project1, title: "Project 1" },
+        { id: 2, image: project1, title: "Project 2" },
+        { id: 3, image: project1, title: "Project 3" },
+        { id: 4, image: project1, title: "Project 4" },
+    ];
 
     return (
-        <div style={{ height: "100%" }}>
+        <Box sx={{ height: "100%", width: "100%" }}>
             <PageLayout image={headerProjects}>
                 <Box sx={parentCardStyle}>
-                    <Card sx={cardStyle2}>
-                        <CardMedia
-                            sx={cardMediaStyle2}
-                            image={project1}
-                            title="project1Image"
-                        />
-                    </Card>
-                    <Card sx={cardStyle2}>
-                        <CardMedia
-                            sx={cardMediaStyle2}
-                            image={project1}
-                            title="project1Image"
-                        />
-                    </Card>
-                    <Card sx={cardStyle2}>
-                        <CardMedia
-                            sx={cardMediaStyle2}
-                            image={project1}
-                            title="project1Image"
-                        />
-                    </Card>
-                    <Card sx={cardStyle2}>
-                        <CardMedia
-                            sx={cardMediaStyle2}
-                            image={project1}
-                            title="project1Image"
-                        />
-                    </Card>
+                    {projectCards.map((project) => (
+                        <CardContent
+                            key={project.id}
+                            sx={{
+                                p: 0,
+                                height: "100%",
+                                "&:last-child": { pb: 0 },
+                            }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={cardMediaStyle}
+                                image={project.image}
+                                title={project.title}
+                                loading="lazy"
+                            />
+                        </CardContent>
+                    ))}
                 </Box>
             </PageLayout>
-        </div>
+        </Box>
     );
 };
