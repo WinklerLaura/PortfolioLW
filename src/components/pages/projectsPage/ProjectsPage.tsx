fd80ef109b9d606ac176dfc853dc641a976f9fa0
lwin <winkler@cogvis.ai>
2024-11-16 14:38:06 +0100

    adds a ProjectModal

--- src/components/pages/projectsPage/ProjectsPage.tsx
+++ src/components/pages/projectsPage/ProjectsPage.tsx
@@ -6,14 +6,22 @@ import project1 from "../../../assets/projectImages/project1.svg";
 import project2 from "../../../assets/projectImages/project2.svg";
 import project3 from "../../../assets/projectImages/project3.svg";
 import project4 from "../../../assets/projectImages/project4.svg";
+import { useState } from "react";
+import { ProjectsModal } from "./ProjectsModal";
 
 export const ProjectsPage = () => {
+    const [openModal, setOpenModal] = useState(false);
+
     const projectCards = [
         { id: 1, image: project1, title: "Project 1" },
         { id: 2, image: project2, title: "Project 2" },
         { id: 3, image: project3, title: "Project 3" },
         { id: 4, image: project4, title: "Project 4" },
     ];
+
+    const handleClick = () => {
+        setOpenModal(true);
+    };
 
     return (
         <Box sx={{ height: "100%", width: "100%" }}>
@@ -34,10 +42,12 @@ export const ProjectsPage = () => {
                                 image={project.image}
                                 title={project.title}
                                 loading="lazy"
+                                onClick={handleClick}
                             />
                         </CardContent>
                     ))}
                 </Box>
+                <ProjectsModal open={openModal} onOpenModal={setOpenModal} />
             </PageLayout>
         </Box>
     );
